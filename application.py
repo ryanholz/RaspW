1e1aee6f5860110016a98ccf3da6df0e7fe23349
ryanholz <49693249+ryanholz@users.noreply.github.com>
2019-06-27 16:11:04 -0400

    More Changes

--- application.py
+++ application.py
@@ -1,6 +1,28 @@
 from flask import Flask
+import pyodbc
+import json
+
 app = Flask(__name__)
 
 @app.route("/")
-def index():
-    return "<h1>Hello Azure!</h1>"
+def hello():
+    data = {
+        "data": {
+            "TimeStamp": "Reading",
+        }
+    }
+    
+    cnxn = pyodbc.connect('DRIVER={SQL Server};SERVER=testserver1909.database.windows.net;DATABASE=test-database;UID=admin1909;PWD=Password1909')
+    cursor = cnxn.cursor()
+    cursor.execute("SELECT * FROM [dbo].[dataLog]")
+    for row in cursor.fetchall():
+        theString = str(row).replace("(","").replace(")","")
+        theSplit = theString.split(", ")
+        unixTime = float(theSplit[0])
+        theReading = float(theSplit[1])
+        data["data"].update({unixTime:theReading})
+    
+    return(json.dumps(data))
+    
+if __name__ == "__main__":
+    app.run()
